a01e67f98c45d5b7d8778b338c5bb7e89513ca20
mr-scrpt <mr.scrpt@gmail.com>
2021-10-24 16:04:44 +0300

    implemented: competence and dignity section

--- src/data/index.js
+++ src/data/index.js
@@ -5,4 +5,7 @@ exports.data = {
 	phone: JSON.parse(readFileSync("./src/data/phone.json", "utf8")),
 	social: JSON.parse(readFileSync("./src/data/social.json", "utf8")),
 	lang: JSON.parse(readFileSync("./src/data/lang.json", "utf8")),
+	competence: JSON.parse(readFileSync("./src/data/competence.json", "utf8")),
+	principle: JSON.parse(readFileSync("./src/data/principle.json", "utf8")),
+	dignity: JSON.parse(readFileSync("./src/data/dignity.json", "utf8")),
 };
---
+++ src/js/custom/jq-ui.js
@@ -0,0 +1,22 @@
+/* $(document).ready(() => {
+    const width = $(window).width();
+    const collapsible = width > 540 ? false : true;
+    $(".qa-item__inner").accordion({
+        active: false,
+        heightStyle: "content",
+        collapsible: true,
+    });
+
+    $(".menu-slide__inner").accordion({
+        active: false,
+        heightStyle: "content",
+        collapsible,
+    });
+
+    $(".footer__box_slide .footer-cell__item").accordion({
+        active: false,
+        heightStyle: "content",
+        collapsible,
+    });
+});
+ */
